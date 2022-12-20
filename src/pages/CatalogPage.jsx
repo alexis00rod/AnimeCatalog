49b1a51028577dcb061325aca607c1abb2bdab15
alexis00rod <alexis00rodrigo@gmail.com>
2022-12-19 21:55:16 -0300

    Cambie estilos

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,53 +1,69 @@
 import { useState, useEffect } from 'react'
-import { NavLink, useParams } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 import { Main,Card,Loader } from '../components/index'
 import { getCatalog, getGenres } from '../service/fetch'
 
-const GenreLink = ({genre}) => {
-    return (
-        <NavLink to={`${genre ? `/catalog/${genre}` : "/catalog"}`}
-            className={({isActive}) => `debug w-max px-2 py-1 text-slate-100 bg-red-500 rounded-lg duration-300 hover:bg-red-600 ${isActive && "bg-red-700"}`}>
-            {genre ? genre : "All"}
-        </NavLink>
-    )
-}
-
 export const CatalogPage = () => {
-    const {genre} = useParams()
-    const [catalog, setCatalog] = useState([])
     const [loading, setLoading] = useState(false)
-    const [catalogSize, setCatalogSize] = useState(15)
-    const [catalogGenre, setCatalogGenre] = useState("Fantasy")
+    const [catalog, setCatalog] = useState([])
+    const [genres, setGenres] = useState(null)
+    const [catalogPage, setCatalogPage] = useState(1)
+    const [catalogGenre, setCatalogGenre] = useState("All")
     const [catalogSortBy, setCatalogSortBy] = useState("ranking")
     const [catalogSortOrder, setCatalogSortOrder] = useState("asc")
-    const [genres, setGenres] = useState([])
 
     useEffect(() => {
-        getCatalog(catalogSize,catalogGenre,catalogSortBy,catalogSortOrder)
+        // getGenres()
+        // .then(resp => setGenres(resp))
+    },[])
+    
+    useEffect(() => {
+        getCatalog(catalogPage,catalogGenre,catalogSortBy,catalogSortOrder)
         .then(resp => setCatalog(resp))
+        .then(() => window.scrollTo(0,0))
         .finally(() => setLoading(true))
-
-        getGenres()
-        .then(resp => setGenres(resp))
-
-    },[catalogSize,catalogGenre,catalogSortBy,catalogSortOrder])
+    },[catalogPage,catalogGenre,catalogSortBy,catalogSortOrder])
 
     return (
         <Main>
-            {loading
-            ?   <><h2 className='debug px-1 py-1 text-3xl font-bold'>Catalog</h2>
-                <ul className='debug px-1 py-1 w-full h-max flex flex-wrap gap-2'>
-                    <li className='debug'>
-                        <GenreLink />
-                    </li>
-                    {/* {genres.map(element => <li key={element._id}>
-                        <GenreLink genre={element._id} />
-                    </li>)} */}
-                </ul>
-                <ul className='debug px-1 py-1 grow grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2'>
+            {!loading && <Loader />}
+            {loading && <>
+                <div className='px-1 py-1 flex items-center'>
+                    <Link to='/' className='text-xs hover:text-red-500'>Home</Link>
+                    <span className='mx-1'>/</span>
+                    <Link to='/catalog' className='text-xs hover:text-red-500'>Catalog</Link>
+                </div>
+                <div className='w-full px-4 py-4 mb-2 flex flex-wrap gap-4 bg-zinc-300 rounded-lg'>
+                    <div className='select'>
+                        <label htmlFor="genre">Genre</label>
+                        <select name="" id="" defaultValue={catalogGenre} onChange={({target:{value}}) => setCatalogGenre(value)}>
+                            <option value="All">All</option>
+                            <option value="Fantasy">Fantasy</option>
+                        </select>
+                    </div>
+                    <div className='select'>
+                        <label htmlFor="by">Sort by</label>
+                        <select id="by"  defaultValue={catalogSortBy} onChange={({target:{value}}) => setCatalogSortBy(value)}>
+                            <option value="ranking">Ranking</option>
+                            <option value="title">Title</option>
+                        </select>
+                    </div>
+                    <div className='select'>
+                        <label htmlFor="order">Sort order</label>
+                        <select id="order"  defaultValue={catalogSortOrder} onChange={({target:{value}}) => setCatalogSortOrder(value)}>
+                            <option value="asc">Asc</option>
+                            <option value="desc">Desc</option>
+                        </select>
+                    </div>
+                </div>
+                <div className='px-1 py-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4'>
                     {catalog.map(element => <Card key={element._id} content={element} />)}
-                </ul></>
-            :   <Loader />}
+                </div>
+                <div className='px-1 py-1 w-full flex justify-center gap-4'>
+                    {catalogPage > 1 && <button className='w-10 h-10 flex items-center justify-center bg-red-500 text-zinc-100 rounded-lg duration-300 hover:bg-red-600' onClick={() => {setCatalogPage(catalogPage - 1)}} ><i className='fa-solid fa-chevron-left'></i></button>}
+                    <button className='w-10 h-10 flex items-center justify-center bg-red-500 text-zinc-100 rounded-lg duration-300 hover:bg-red-600' onClick={() => {setCatalogPage(catalogPage + 1)}} ><i className='fa-solid fa-chevron-right'></i></button>
+                </div>
+            </>}
         </Main>
     )
 }
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,34 @@
-import { Main } from "../components/Main"
+import { useEffect } from "react"
+import { useState } from "react"
+import { Link } from "react-router-dom"
+import { Main, Card } from '../components/index'
+import { getSearchAnime } from "../service/fetch"
 
 export const SearchPage = () => {
+    const [animeFind, setAnimeFind] = useState([])
+    const [search, setSearch] = useState("")
+
+    const searchAnime = e => {
+        e.preventDefault()
+        search !== "" && getSearchAnime(search).then(resp => setAnimeFind(resp))
+    }
+
     return (
         <Main>
-            <h2 className='debug px-2 pt-1 pb-2 text-3xl font-bold'>Search</h2>
+            <div className='px-1 py-1 flex items-center'>
+                <Link to='/' className='text-xs hover:text-red-500'>Home</Link>
+                <span className='mx-1'>/</span>
+                <Link to='/search' className='text-xs hover:text-red-500'>Search</Link>
+            </div>
+            <div className="w-full mb-2 px-4 py-4 bg-zinc-300 rounded-lg">
+                <form className="flex items-center bg-zinc-200 border border-zinc-100 rounded-lg" onSubmit={searchAnime}>
+                    <input type="text" className="h-10 px-3 grow bg-transparent outline-none placeholder:text-zinc-800" onChange={({target:{value}}) => setSearch(value)} placeholder="Search anime" />
+                    <button type="submit" className="w-10 h-10 flex items-center justify-center text-zinc-500 duration-300 hover:text-zinc-900"><i className="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+            </div>
+            <div className='px-1 py-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4'>
+                {animeFind.map(element => <Card key={element._id} content={element} />)}
+            </div>
         </Main>
     )
 }
