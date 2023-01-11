bc1ad062f655116c52d08fa39b34c17ec9ead580
alexis00rod <alexis00rodrigo@gmail.com>
2023-01-11 15:02:00 -0300

    Agrege funcion para filtrar catalogo

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
-import { useParams } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { useNavigate, useParams } from "react-router-dom"
 import { Loader, Main, Pagination, Catalog } from '../components/index'
 import { useCatalog } from '../hooks/useCatalog'
 import { useGenres } from "../hooks/useGenres"
@@ -6,46 +7,60 @@ import { useGenres } from "../hooks/useGenres"
 export const HomePage = () => {
   const {page} = useParams()
   const {genres} = useGenres()
-  const {loading, catalog} = useCatalog()
+  const {loading, catalog, setCatalogFilter} = useCatalog()
+  const [formFilter, setFormFilter] = useState({
+    title: "",genre: "All", by: "ranking", order: "asc"
+  })
+  const navigate = useNavigate()
+
+  const handleFilter = ({target:{name,value}}) => {
+    setFormFilter({...formFilter,[name]:value})
+  }
+
+  const filterCatalog = e => {
+    e.preventDefault()
+    setCatalogFilter(formFilter)
+    navigate('/')
+  }
 
   if(!loading) return <Loader />
 
   return (
     <Main>
       {/* Filter */}
-      <form className='w-full my-2 px-4 py-4 flex flex-col gap-4 items-center bg-zinc-700 rounded-lg'>
-        <input type="text" name="title" id="title" className='input-text' placeholder='Enter name anime...'/>
+      <form className='w-full my-2 px-4 py-4 flex flex-col gap-4 items-center bg-zinc-700/50 rounded-lg' onSubmit={filterCatalog}>
+        <input type="text" name="title" id="title" className='input-text' placeholder='Enter name anime...' onChange={handleFilter}/>
         <div className="w-full flex flex-wrap gap-2">
           <div className='select'>
             <label htmlFor="genre">Genre</label>
-            <select name="genre" id="genre">
+            <select name="genre" id="genre" onChange={handleFilter}>
               <option value="All">All</option>
-              {genres.map(e => <option value={e._id}>{e._id}</option>)}
+              {genres.map(e => <option value={e._id} key={e._id}>{e._id}</option>)}
             </select>
           </div>
           <div className='select'>
             <label htmlFor="by">Sort by</label>
-            <select name="by" id="by">
+            <select name="by" id="by" onChange={handleFilter}>
               <option value="ranking">Ranking</option>
               <option value="title">Title</option>
             </select>
           </div>
           <div className='select'>
             <label htmlFor="order">Sort order</label>
-            <select name="order" id="order">
+            <select name="order" id="order" onChange={handleFilter}>
               <option value="asc">Asc</option>
               <option value="desc">Desc</option>
             </select>
           </div>
         </div>
-        <button className="btn btn-form">Search</button>
+        <button type='submit' className="btn btn-form">Search</button>
       </form>
   
       {/* Catalog */}
-      {/* <Catalog catalog={catalog.data} /> */}
+      <Catalog catalog={catalog.data} />
   
       {/* Pagination */}
-      {/* <Pagination totalPage={catalog.meta.totalPage} page={page} /> */}
+      <Pagination totalPage={catalog.meta.totalPage} page={page} />
     </Main>
   )
 }
--- src/service/fetch.js
+++ src/service/fetch.js
@@ -3,13 +3,16 @@ import { options } from './keys.js'
 // Funcion para obtener la lista completa de animes
 export const getCatalog = async ({title,genre,by,order},page) => {
   const url = genre === "All"
-      ?   `https://anime-db.p.rapidapi.com/anime?page=${page ? page : 1}&size=24&sortBy=${by}&sortOrder=${order}`
-      :   `https://anime-db.p.rapidapi.com/anime?page=${page ? page : 1}&size=24&genres=${genre}&sortBy=${by}&sortOrder=${order}`
-  
+    ? title
+      ? `https://anime-db.p.rapidapi.com/anime?page=${page ? page : 1}&size=24&search=${title}&sortBy=${by}&sortOrder=${order}`
+      : `https://anime-db.p.rapidapi.com/anime?page=${page ? page : 1}&size=24&sortBy=${by}&sortOrder=${order}`
+    : title
+      ? `https://anime-db.p.rapidapi.com/anime?page=${page ? page : 1}&size=24&search=${title}&genres=${genre}&sortBy=${by}&sortOrder=${order}`
+      : `https://anime-db.p.rapidapi.com/anime?page=${page ? page : 1}&size=24&genres=${genre}&sortBy=${by}&sortOrder=${order}`
+
   const request = await fetch(url,options)
   const json = await request.json()
-  const {data} = json
-  return data
+  return json
 }
 
 // Funcion para obtener los generos
